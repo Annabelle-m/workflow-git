62d759cea5fc6ba09afc75f3532ca28fd8a572a4
Annabelle Megy <annabelle.megy@gmail.com>
2025-09-05 15:26:23 +0200

    second modification script something.js

--- something.js
+++ something.js
@@ -1,7 +1,6 @@
 // Some useless JavaScript code
-let min = 5;
-let max = 25;
 
-for (let index = min; index <= max; index++) {
+
+for (let index = 0; index <= 10; index++) {
     console.log(index);
 }
